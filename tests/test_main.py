3ca87cebba8fa779ca5951dce022e2f8a48f8eb9
berkaycanozcelik <brkycn10@gmail.com>
2024-10-12 12:54:29 +0300

    added new test for the /predict EP (WIP)

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,5 +6,21 @@ client = TestClient(app)
 
 def test_root():
     response = client.get("/")
-    print("Response JSON:", response.json())
+    assert response.status_code == 200
     assert response.json() == {"message": "Hello HUK"}
+
+#TODO: Work In Process
+#First need to finish the /predict EP
+def test_predict():
+    # Sample input data for the /predict/ endpoint
+    sample_input = {
+        "text": "I love this product, it’s fantastic!"
+    }
+
+    # Sending a POST request to the /predict/ endpoint
+    response = client.post("/predict/", json=sample_input)
+
+    assert response.status_code == 200
+    # Asserting that the response contains a "prediction" field
+    response_json = response.json()
+    assert "prediction" in response_json
